065bb9d8ca71433a64a73c587112cb372a9e5eea
Yuji Hayashi <arthuryuji@hotmail.com>
2015-06-21 02:36:22 -0300

    Sidebar + interação

--- _site/assets/js/bootstrap_site.js
+++ _site/assets/js/bootstrap_site.js
@@ -1,6 +1,21 @@
 $(function() {
+	$('.nav-left > li').each( function () {
+		var thisLi = $(this);
+		var child = $(this).find('> ul');
+		if (thisLi.has('ul')) {
+			thisLi.find('> a').click( function () {
+				if (thisLi.hasClass('open')) {
+					child.stop(true, true).delay(100).hide({duration: 500, queue: true, easing: 'easeInOutExpo'});
+					thisLi.removeClass('open');
+				} else {
+					child.stop(true, true).delay(100).show({duration: 500, queue: true, easing: 'easeInOutExpo'});
+					thisLi.addClass('open');
 
-    
+				}
+			});
+		}
+	});
+
 // $('.ver-imagem').prettyPhoto();
 // $('a[rel^="prettyPhoto"]').prettyPhoto();
 });
